af62d60252f2cde50225aee929d739226831bb6d
Flavio Alves <flavio.alves84@gmail.com>
2023-03-27 17:20:28 -0300

    update

--- src/br/com/alura/rh/service/ReajusteService.java
+++ src/br/com/alura/rh/service/ReajusteService.java
@@ -11,7 +11,8 @@ public class ReajusteService {
         BigDecimal salarioAtual = funcionario.getSalario();
         BigDecimal percentualReajuste = aumento.divide(salarioAtual, RoundingMode.HALF_UP);
         if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
-            throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
+            throw new ValidacaoException(
+                    "Reajuste nao pode ser superior a 40% do salario!");
         }
         BigDecimal salarioReajustado = salarioAtual.add(aumento);
         funcionario.atualizarSalario(salarioReajustado);
